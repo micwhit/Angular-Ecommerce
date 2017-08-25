4dbd77099e0d76c931e7c572f854573c53f98f36
Micwhit <Micweb1980>
2017-08-25 12:00:10 -0500

    Angular Project

---
+++ scripts/routes.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../node_modules/angular/angular-route.js" />
+/// <reference path="../node_modules/angular/angular.js" />
+
+(function () {
+    angular.module('store').config(function ($routeProvider) {
+        $routeProvider.when('/filterColors', { templateUrl: '/templates/filters/productColor.html' }); //when (variable name--add the forward slash, {object})
+        $routeProvider.when('/filterSize', { templateUrl: '/templates/filters/productSize.html' });
+        $routeProvider.when('/productReview', { templateUrl: '/templates/products/productReview.html' });
+        $routeProvider.when('/productTemple', { templateUrl: '/templates/products/productTemplate.html' });
+    });
+})();
